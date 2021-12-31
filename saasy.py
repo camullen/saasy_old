e2dd956401a5fe41951ed77fcd26222d52f10097
Cam Mullen <cameron.mullen@gmail.com>
2021-12-30 21:33:42 -0800

    Begin to create logic for merging intervals

--- saasy.py
+++ saasy.py
@@ -18,6 +18,8 @@ import pandas as pd
 #    - start_date - the start date of the contract (as pandas Timestamps)
 #    - end_date - the end date of the contract (as pandas Timestamps)
 
+DEFAULT_TOLERANCE = 3
+
 
 def bookings_to_arr(bookings):
     intervals = bookings_to_intervals(bookings)
@@ -28,15 +30,109 @@ def bookings_to_intervals(bookings):
 
     interval_list = []
 
-    for index, row in bookings.sort_values(by=["customer_id", "start_date"]).iterrows():
-        interval_list.append(row)
-        if index == 2:
-            interval_list.append(row)
-        # print(f"Index: {index}")
-        # print(bookings.loc[index])
-        # print("\n")
+    for cust_id, group in bookings.sort_values(by="start_date").groupby("customer_id"):
+        # interval_list.append(row)
+        # if index == 2:
+        #     interval_list.append(row)
+        # print(f"customer_id: {cust_id}\ngroup:\n{group}\n")
+        booking_group_to_intervals(group)
 
     return pd.DataFrame(data=interval_list)
+
+
+# TODO: verify integrity of input data
+# Remember that we are receiving a booking group sorted by start_date (ascending)
+def booking_group_to_intervals(booking_group, tolerance=DEFAULT_TOLERANCE):
+    intervals = []
+    for index, curr_booking in booking_group.itercurr_bookings():
+        # If this is the first booking, then add to intervals and go to next booking
+        if len(intervals) == 0:
+            intervals.append(
+                {
+                    "start_date": curr_booking["start_date"],
+                    "end_date": curr_booking["end_date"],
+                    "customer_id": curr_booking["customer_id"],
+                    "prior_arr": 0,
+                    "arr": curr_booking["arr"],
+                }
+            )
+        # If the bookings are contiguious
+        elif is_contiguous(intervals[-1], curr_booking, tolerance=tolerance):
+            # if bookings are contiguous and have the same ARR, extend previous
+            # interval and move to next booking
+            if intervals[-1]["arr"] == curr_booking["arr"]:
+                intervals[-1]["end_date"] = curr_booking["end_date"]
+
+            # The ARR is different, so we need a new interval
+            # TODO: decide how to handle gaps between previous end_date and new start_date
+            else:
+                intervals.append(
+                    {
+                        "start_date": curr_booking["start_date"],
+                        "end_date": curr_booking["end_date"],
+                        "customer_id": curr_booking["customer_id"],
+                        "prior_arr": intervals[-1]["arr"],
+                        "arr": curr_booking["arr"],
+                    }
+                )
+
+        # If the next booking starts significantly (we've already checked for
+        # (tolerance above) after the last booking ends we need an interval
+        # with 0 ARR in between
+        elif curr_booking["start_date"] > intervals[-1]["end_date"]:
+            # Add churn gap to intervals
+            intervals.append(
+                {
+                    "start_date": intervals[-1]["end_date"],
+                    "end_date": curr_booking["start_date"],
+                    "customer_id": curr_booking["customer_id"],
+                    "prior_arr": intervals[-1]["arr"],
+                    "arr": 0,
+                }
+            )
+            # Add current booking to intervals
+            intervals.append(
+                {
+                    "start_date": curr_booking["start_date"],
+                    "end_date": curr_booking["end_date"],
+                    "customer_id": curr_booking["customer_id"],
+                    "prior_arr": intervals[-1]["arr"],
+                    "arr": curr_booking["arr"],
+                }
+            )
+        # If not contiguous, or the start date is after the previous end date,
+        # there are overlapping bookings and we need to splice something in
+        # TODO: Handle edge case where the the start date of the previous interval
+        # equals that of the current booking
+
+        # Check if they are completely overlapping and up the ARR and expand the bounds
+        elif ts_equal(curr_booking["start_date"], intervals[-1]["start_date"], tolerance=tolerance) and ts_equal(
+            curr_booking["end_date"], intervals[-1]["end_date"], tolerance=tolerance
+        ):
+            intervals[-1]["start_date"] = min(intervals[-1]["start_date"], curr_booking["start_date"])
+            intervals[-1]["end_date"] = max(intervals[-1]["end_date"], curr_booking["end_date"])
+            intervals[-1]["arr"] += curr_booking["arr"]
+
+        # Check if the start is overlapping and then shorten the existing interval and add the new (non-overlapping) one
+        elif ts_equal(curr_booking["start_date"], intervals[-1]["start_date"], tolerance=tolerance):
+            intervals[-1]["end_date"] = min(intervals[-1]["end_date"], curr_booking["end_date"])
+            intervals[-1]["arr"] += curr_booking["arr"]
+            intervals.append(
+                {
+                    "start_date": intervals[-1]["end_date"],
+                    "end_date": max(intervals[-1]["end_date"], curr_booking["end_date"])
+                    # Need to figure out which ARR to put into the non-overlapping segment
+                }
+            )
+
+
+def is_contiguous(prev_interval, next_interval, tolerance=DEFAULT_TOLERANCE):
+    return ts_equal(next_interval["start_date"], prev_interval["end_date"], tolerance=tolerance)
+
+
+def ts_equal(ts_a, ts_b, tolerance=DEFAULT_TOLERANCE):
+    day_gap = (ts_a - ts_b).days
+    return abs(day_gap) <= tolerance
 
 
 test_bookings = pd.DataFrame.from_records(
